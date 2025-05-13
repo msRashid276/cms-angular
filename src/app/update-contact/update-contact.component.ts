849a80d1231c5075e1f6992f581a458356ee939a
msRashid276 <rashidmohammed276@gmail.com>
2025-05-13 23:30:44 +0530

    components created , route creation started

---
+++ src/app/update-contact/update-contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-contact',
+  imports: [],
+  templateUrl: './update-contact.component.html',
+  styleUrl: './update-contact.component.css'
+})
+export class UpdateContactComponent {
+
+}
